581a1209d2c33bff06f952a6c41775d90bceb9e7
Peter Walker <peterw@magmadigital.co.uk>
2018-06-05 11:05:23 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+<?php 
+require ('classes/queries/common.php');?>
+
+<div class="container">
+
+	<div class="alert alert-primary" role="alert" style="margin-top: 30px;">
+		<h1>This is a web page</h1>
+	</div>
+
+	<table class="table table-striped">
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Email</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php foreach ($records as $row) { ?>
+				<tr>
+					<td><?php echo $row['name']?></td>
+					<td><?php echo $row['email']?></td>
+				</tr>
+			<?php } ?>
+		</tbody>
+	</table>
+
+</div>
+
+
+
+
+	
+
+	
